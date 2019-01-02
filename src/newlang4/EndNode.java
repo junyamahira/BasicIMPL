ba5b106d076703b383e1fa7c6d3e0c068eae6a1f
junyamahira <c0116278ba@edu.teu.ac.jp>
2019-01-02 16:14:29 +0900

    add Endnode

--- src/newlang4/EndNode.java
+++ src/newlang4/EndNode.java
@@ -1,5 +1,40 @@
 package newlang4;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public class EndNode extends Node {
+	static final Set<LexicalType> FIRST =
+			new HashSet<LexicalType>(Arrays.asList(LexicalType.END));
+
+	public EndNode(Environment env) {
+		super(env);
+		type = NodeType.END;
+	}
+
+	public static boolean isMatch(LexicalType type) {
+		return FIRST.contains(type);
+	}
+
+	public static Node getHandler(Environment env) {
+		return new EndNode(env);
+	}
+
+	@Override
+	public boolean Parse() throws Exception {
+		if (env.getInput().expect(LexicalType.END)) {
+			env.getInput().get();
+			return true;
+		}else {
+			throw new Exception("ENDが見当たりません");
+		}
+	}
+
+	@Override
+	public String toString() {
+		return "END";
+	}
+
 
 }

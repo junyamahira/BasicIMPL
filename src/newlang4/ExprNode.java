983e76484c57eb16c9a523869862f584a81cecd0
junyamahira <c0116278ba@edu.teu.ac.jp>
2019-01-14 19:00:24 +0900

    add ExprListNode Class

--- src/newlang4/ExprNode.java
+++ src/newlang4/ExprNode.java
@@ -1,5 +1,13 @@
 package newlang4;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public class ExprNode extends Node {
+	static final Set<LexicalType> FIRST =
+			new HashSet<LexicalType>(Arrays.asList(LexicalType.SUB,
+					LexicalType.LP, LexicalType.NAME, LexicalType.INTVAL,
+					LexicalType.DOUBLEVAL, LexicalType.LITERAL));
 
 }
